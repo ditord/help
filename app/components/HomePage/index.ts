ce9ad32a20dcea8be6babe5f31dab538f6fab72f
Artur Papyan <angry.root@gmail.com>
2025-05-06 21:59:31 +0400

    Prepare deployment

--- app/components/HomePage/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./HeroSection";
-export * from "./PartnersSection";
-export * from "./AboutUsSection";
-export * from "./BlogsSection";
-export * from "./HelpSection";
-export * from "./AdvertSection";
-
--- app/elements/index.tsx
+++
@@ -1,3 +0,0 @@
-export * from "./ClipPathPanel";
-export * from "./WhatsappIcon";
-export * from "./PhoneIcon";
